6c4d595d97105315a48c358d268f37435d3f18ee
Robert Levenbach <R.W.Levenbach@student.tudelft.nl>
2017-11-27 21:42:37 +0100

    button changeing

--- WebEnData Assignment/app.js
+++ WebEnData Assignment/app.js
@@ -1,23 +1,21 @@
 var main = function () {
 
-
-         
-
-
-
 	 };
 
+function changeText() {
+	var date= new Date();
+	text = String(date);
+
+	document.getElementById('pText').innerHTML=date.toDateString();
+	document.getElementById('Hour').innerHTML= date.getHours();
+	document.getElementById('Minute').innerHTML=date.getMinutes();
+	document.getElementById('Second').innerHTML=date.getSeconds();
+}
+		
 
 
+function changePicture(image){
+	  document.getElementById("imgClickAndChange").src = image;
+}
 
-	    function changeText() {
-	    	 	var date= new Date();
-	    		text = String(date);
-
-                document.getElementById('pText').innerHTML=date.toDateString();
-                document.getElementById('Hour').innerHTML= date.getHours();
-                document.getElementById('Minute').innerHTML=date.getMinutes();
-                document.getElementById('Second').innerHTML=date.getSeconds();
-
-            }
 $(document).ready(main);
