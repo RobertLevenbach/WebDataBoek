1c866a627181a88e526e2db37161bb9d19969028
Robert Levenbach <R.W.Levenbach@student.tudelft.nl>
2017-11-27 20:25:01 +0100

    Time Last Drink Complete

--- WebEnData Assignment/app.js
+++ WebEnData Assignment/app.js
@@ -10,8 +10,14 @@ var main = function () {
 
 
 
-	    function changeText(text) {
-                document.getElementById('pText').innerHTML=text;
+	    function changeText() {
+	    	 	var date= new Date();
+	    		text = String(date);
+
+                document.getElementById('pText').innerHTML=date.toDateString();
+                document.getElementById('Hour').innerHTML= date.getHours();
+                document.getElementById('Minute').innerHTML=date.getMinutes();
+                document.getElementById('Second').innerHTML=date.getSeconds();
 
             }
 $(document).ready(main);
